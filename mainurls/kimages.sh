fa48288eee112e4ce6873d349f1b6b894276dc21
leotechnosoftgithub <64788555+leotechnosoftgithub@users.noreply.github.com>
2020-06-03 17:06:15 +0530

    update
    update

--- mainurls/kimages.sh
+++ mainurls/kimages.sh
@@ -4,4 +4,4 @@ key=/root/.ssh/mst.pem
 curl -o ./ips/kimages  http://18.207.24.43/kimages
 for i in `cat xaa/gaa`;do ip=`shuf -n 1 ./ips/kimages`; echo $ip; ssh -o StrictHostKeyChecking=no -i $key root@$ip ./alpha8.sh "\"$i\"" "\"$dat\"";done
 echo Subject: Job job1 completed | sendmail sheetal.pant@leotechnosoft.net
-#screen -X quit
+screen -X quit
