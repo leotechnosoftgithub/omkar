9ecc273a3cd3603d08d00f6d145a0ac0a5c54224
leotechnosoftgithub <support@leotechnosoft.net>
2020-06-08 11:09:28 +0000

    first

---
+++ videourls/May/21-05-2020/scripts/Swapnil_21data15.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+dat=$1
+key=/root/.ssh/mst.pem
+curl -o ./ips/videoips  http://18.207.24.43/videoips
+for i in `cat ./videourls/May/21-05-2020/urls/15`;do ip=`shuf -n 1 ./ips/videoips`; echo $ip; ssh -o StrictHostKeyChecking=no -i $key root@$ip ./alpha5.sh "\"$i\"" "\"$dat\"";done
+echo Subject: Job Swapnil_21data15.sh completed | sendmail sheetal.pant@leotechnosoft.net
+screen -X quit
---
+++ videourls/May/22-05-2020/scripts/swapnil22may29.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+dat=$1
+key=/root/.ssh/mst.pem
+curl -o ./ips/videoips  http://18.207.24.43/videoips
+for i in `cat ./videourls/May/22-05-2020/urls/29`;do ip=`shuf -n 1 ./ips/videoips`; echo $ip; ssh -o StrictHostKeyChecking=no -i $key root@$ip ./alpha5.sh "\"$i\"" "\"$dat\"";done
+echo Subject: Job swapnil22may29.sh completed | sendmail sheetal.pant@leotechnosoft.net
+screen -X quit
