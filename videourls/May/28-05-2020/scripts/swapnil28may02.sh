83a7c0e10e12816d5929e47614762b269e3ab8c3
leotechnosoftgithub <support@leotechnosoft.net>
2020-06-11 07:47:28 +0000

    test

---
+++ videourls/May/28-05-2020/scripts/swapnil28may02.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+dat=$1
+key=/root/.ssh/mst.pem
+curl -o ./ips/videoips  http://18.207.24.43/videoips
+for i in `cat ./videourls/May/28-05-2020/urls/02`;do ip=`shuf -n 1 ./ips/videoips`; echo $ip; ssh -o StrictHostKeyChecking=no -i $key root@$ip ./alpha5.sh "\"$i\"" "\"$dat\"";done
+echo Subject: Job swapnil28may02.sh completed | sendmail sheetal.pant@leotechnosoft.net
+screen -X quit
