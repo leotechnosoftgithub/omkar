12156cdd6bee8ba2f676c6ad80a941267f197b00
leotechnosoftgithub <64788555+leotechnosoftgithub@users.noreply.github.com>
2020-07-21 16:52:35 +0530

    update
    update

--- mainurls/videos.sh
+++ mainurls/videos.sh
@@ -2,6 +2,6 @@
 dat=$1
 key=/root/.ssh/mst.pem
 curl -o ./ips/videoips  http://18.207.24.43/videoips
-for i in `cat xaa/gaa`;do ip=`shuf -n 1 ./ips/videoips`; echo $ip; ssh -o StrictHostKeyChecking=no -i $key root@$ip ./alpha5.sh "\"$i\"" "\"$dat\"";done
+for i in `cat xaa/gaa`;do ip=`shuf -n 1 ./ips/videoips`; echo $ip; ssh -o StrictHostKeyChecking=no -i $key root@$ip ./swapnil.sh "\"$i\"" "\"$dat\"";done
 echo Subject: Job job1 completed | sendmail sheetal.pant@leotechnosoft.net
 screen -X quit
