f18727b5d5493d6b6b90f282b76571dc6ad93818
leotechnosoftgithub <64788555+leotechnosoftgithub@users.noreply.github.com>
2020-07-26 13:41:14 +0530

    sdf
    sdf

--- mainurls/weburl.sh
+++ mainurls/weburl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dat=$1
 key=/root/.ssh/mst.pem
-curl -o ./ips/weburl  http://18.207.24.43/weburl
+curl -o ./ips/http://67.205.178.30/ips/weburl
 for i in `cat xaa/gaa`;do ip=`shuf -n 1 ./ips/weburl`; echo $ip; ssh -o StrictHostKeyChecking=no -i $key root@$ip ./alpha2.sh "\"$i\"" "\"$dat\"";done
 echo Subject: Job job1 completed | sendmail sheetal.pant@leotechnosoft.net
 screen -X quit
