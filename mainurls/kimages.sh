009d2495cbd4f20e867f3091a819fadd6c151930
leotechnosoftgithub <64788555+leotechnosoftgithub@users.noreply.github.com>
2020-08-11 23:36:34 +0530

    sdf
    sdf

--- mainurls/kimages.sh
+++ mainurls/kimages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dat=$1
 key=/root/.ssh/mst.pem
-curl -o ./ips/kimages  http://18.207.24.43/kimages
+#curl -o ./ips/kimages  http://18.207.24.43/kimages
 for i in `cat xaa/gaa`;do ip=`shuf -n 1 ./ips/kimages`; echo $ip; ssh -o StrictHostKeyChecking=no -i $key root@$ip ./iarch.sh "\"$i\"" "\"$dat\"";done
 screen -X quit
