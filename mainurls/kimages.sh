41f3b93993d3f5940855c11f9437713f68b2ce79
leotechnosoftgithub <64788555+leotechnosoftgithub@users.noreply.github.com>
2020-08-11 23:34:30 +0530

    sdf
    sdf

--- mainurls/kimages.sh
+++ mainurls/kimages.sh
@@ -2,6 +2,6 @@
 dat=$1
 key=/root/.ssh/mst.pem
 curl -o ./ips/kimages  http://18.207.24.43/kimages
-for i in `cat xaa/gaa`;do ip=`shuf -n 1 ./ips/kimages`; echo $ip; ssh -o StrictHostKeyChecking=no -i $key root@$ip ./alpha8.sh "\"$i\"" "\"$dat\"";done
+for i in `cat xaa/gaa`;do ip=`shuf -n 1 ./ips/kimages`; echo $ip; ssh -o StrictHostKeyChecking=no -i $key root@$ip ./iarch.sh "\"$i\"" "\"$dat\"";done
 echo Subject: Job job1 completed | sendmail sheetal.pant@leotechnosoft.net
 screen -X quit
